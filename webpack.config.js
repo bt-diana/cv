28a4dfc4bc5fc8d011256120c74d416edd5b9619
Diana Butiakova <butyakova01@gmail.com>
2025-01-21 22:29:27 +0600

    refactor: use js instead of ts

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: process.env.NODE_ENV || 'development',
     entry: {
-        bundle: path.resolve(__dirname, 'src\/scripts\/index.ts'),
+        bundle: path.resolve(__dirname, 'src\/scripts\/index.js'),
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
