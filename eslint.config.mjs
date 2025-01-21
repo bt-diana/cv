7382a67f366d0e9f16deef19a42293a89622304b
Diana Butiakova <butyakova01@gmail.com>
2025-01-21 17:07:38 +0600

    init: configure eslint for html

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import html from "@html-eslint/eslint-plugin";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -9,4 +9,8 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"],
+  }
 ];
