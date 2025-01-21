5fda270b9d0628a1f00f898f0be3e0e9c1c3a8b6
Diana Butiakova <butyakova01@gmail.com>
2025-01-21 12:24:15 +0600

    init: configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src\/scripts\/index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        clean: true,
+    },
+    devtool: process.env.NODE_ENV ? false : 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|txt|csv|mmdb|jpg|png|svg)$/i,
+                type: 'asset/resource',
+            },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'], exclude: /node_modules/ },
+            { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/\index.html'),
+        }),
+    ],
+};
