536f7dddaee55e7179cf85a3e33b65c9a349e6ca
Diana Butiakova <butyakova01@gmail.com>
2025-01-22 13:26:31 +0600

    feat: make certification link as icon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,1 @@
 import '../styles/index.css';
-
-function configureProfileBack() {
-    const aside = document.querySelector(".profile__aside");
-    const info = document.querySelector(".profile__info");
-    const profileBack = document.querySelector(".profile__back");
-    console.log(aside.offsetLeft > info.offsetLeft, aside.offsetLeft, info.offsetLeft)
-    if (aside.offsetLeft > info.offsetLeft) {
-        if (!profileBack.classList.contains("profile__back--horizontal")) {
-            profileBack.classList.add("profile__back--horizontal");
-        }
-    } else {
-        profileBack.classList.remove("profile__back--horizontal");
-    }
-}
-
-configureProfileBack();
-window.onresize = configureProfileBack
-
