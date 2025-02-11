d251a4a3fd8aa2640ec72f01e00839b091c42668
Diana Butiakova <butiakova.diana@gmail.com>
2025-02-11 11:51:22 +0600

    refactor: invoke filloutDocument funcion on changing language

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,12 @@
 import '../styles/index.css';
-import './setupLocalization.js';
+import setLocalization from './setLocalization.js';
+
+const langOptions = document.querySelectorAll('.localization-menu__item');
+langOptions.forEach((langOption) => {
+    langOption.addEventListener('click', (e) => {
+        setLocalization(e.target.dataset.lang);
+    });
+})
+
+const userLang = navigator.language ?? navigator.userLanguage;
+setLocalization(['ru', 'be', 'ky', 'kk'].includes(userLang) ? 'ru' : 'en');
