ba22a9516515f52484b8aaa9117ecabe9d41cbf2
Diana Butiakova <butiakova.diana@gmail.com>
2025-02-12 15:05:59 +0600

    feat: add updating localization text

--- src/scripts/filloutDocument.js
+++ src/scripts/filloutDocument.js
@@ -1,3 +1,8 @@
-export default function filloutDocument(langCode) {
+import data from '../assets/data/data.json';
 
+export default function filloutDocument(langCode) {
+    Object.entries(data[langCode]).forEach(([selector, text]) => {
+        const elements = document.querySelectorAll(selector);
+        elements?.forEach((element) => {element.innerText = text});
+    })
 }
