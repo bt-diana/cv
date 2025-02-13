41d68b8be13c2492435d4d6f640ae43589744de4
Diana Butiakova <butiakova.diana@gmail.com>
2025-02-13 12:19:00 +0600

    feat: when changing localization go to another page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,1 @@
 import '../styles/index.css';
-import setLocalization from './setLocalization.js';
-
-const langOptions = document.querySelectorAll('.localization-menu__item');
-langOptions.forEach((langOption) => {
-    langOption.addEventListener('click', (e) => {
-        setLocalization(e.target.dataset.lang);
-    });
-})
-
-const userLang = navigator.language ?? navigator.userLanguage;
-setLocalization(['ru', 'be', 'ky', 'kk'].includes(userLang) ? 'ru' : 'en');
